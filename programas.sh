fdd25061de9b9ad831374a1912425381aeeadba2
crisvegadiaz <88862218+crisvegadiaz@users.noreply.github.com>
2022-09-04 12:18:26 -0300

    Update programas.sh
    actualización

--- programas.sh
+++ programas.sh
@@ -1,10 +1,33 @@
 #! /bin/bash
 
-echo 'Debes usar sudo'
-arry=('git' 'tilix' 'htop' 'neofetch' 'vlc')
-for i in "${arry[@]}"
+echo -e "\e[1;34m Debe usar Sudo\e[0m"
+
+# DNF
+arry1=('git' 'tmux' 'htop' 'neofetch' 'tree' 'locate' 'curl' 'neovim')
+
+for i in "${arry1[@]}"
 do
-  dnf install ${i} -y
-  echo "<-------------------->"
+    echo -e "\e[31m  Programa a Intalar ${i}\e[0m"
+    dnf install ${i} -y
+    echo -e "\e[1;34m <------------------------>\e[0m"
 done
 
+# SNAP
+arry2=('bottom' 'speedtest-cli' 'cpufetch')
+
+for i in "${arry2[@]}"
+do
+    echo -e "\e[31m  Programa a Intalar ${i}\e[0m"
+    snap install ${i}
+    echo -e "\e[1;34m <------------------------>\e[0m"
+done
+
+# FLATPAK
+arry3=('telegram')
+
+for i in "${arry3[@]}"
+do
+    echo -e "\e[31m  Programa a Intalar ${i}\e[0m"
+    flatpak install ${i} -y
+    echo -e "\e[1;34m <------------------------>\e[0m"
+done
