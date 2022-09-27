c71430e12bc2906ac902c04c820e9dfc22d515a0
crisvegadiaz <88862218+crisvegadiaz@users.noreply.github.com>
2022-09-27 20:03:28 -0300

    Update programas.sh

--- programas.sh
+++ programas.sh
@@ -1,33 +1,88 @@
 #! /bin/bash
 
-echo -e "\e[1;34m Debe usar Sudo\e[0m"
+#Paleta de colores texto
+function texColor(){
+    
+    mensaje="$1"
+    tipo="$2"
+    
+    arry1=(31 32 33 34 35)
+    arry2=('red' 'green' 'yellow' 'blue' 'purple')
+    
+    for (( c=0; c<=${#arry2[@]}; c++ ))
+    do
+        if [[ $tipo == ${arry2[c]} ]]; then
+            echo -en "\e[${arry1[c]}m${mensaje}\e[0m"
+        fi
+    done
+}
 
-# DNF
+echo $0
+
+texColor "
+█ █▄░█ █▀ ▀█▀ ▄▀█ █░░ ▄▀█ █▀█   █▀█ █▀█ █▀█ █▀▀ █▀█ ▄▀█ █▀▄▀█ ▄▀█ █▀
+█ █░▀█ ▄█ ░█░ █▀█ █▄▄ █▀█ █▀▄   █▀▀ █▀▄ █▄█ █▄█ █▀▄ █▀█ █░▀░█ █▀█ ▄█
+" purple
+printf "\n\n"
+texColor "Debe usar Sudo:" yellow
+printf "\n\n"
+
+texColor "▄▄ ▄▄ ▄▄ ▄▄ ▄▄ ▄▄ ▄▄ ▄▄ ▄▄ ▄▄ ▄▄ ▄▄ ▄▄ ▄▄ ▄▄ ▄▄ ▄▄ ▄▄ ▄▄ ▄▄ ▄▄ ▄▄ ▄▄ ▄▄ ▄▄ ▄▄ ▄▄ ▄▄ " blue
+printf "\n"
+texColor "
+█▀▄ █▄░█ █▀▀
+█▄▀ █░▀█ █▀░
+" purple
+
+texColor ""
 arry1=('git' 'tmux' 'htop' 'neofetch' 'tree' 'locate' 'curl' 'neovim')
 
 for i in "${arry1[@]}"
 do
-    echo -e "\e[31m  Programa a Intalar ${i}\e[0m"
+    printf "\n\n"
+    texColor " Programa a Intalar ${i}" green
+    printf "\n\n"
     dnf install ${i} -y
-    echo -e "\e[1;34m <------------------------>\e[0m"
+    printf "\n\n"
+    texColor "_̶_̶_̶_̶_̶_̶_̶_̶_̶_̶_̶_̶_̶_̶_̶_̶_̶_̶_̶_̶_̶_̶_̶_̶_̶_̶_̶_̶_̶_̶_̶_̶_̶_̶_̶_̶_̶_̶_̶_̶_̶_̶_̶_̶_̶_̶_̶_̶_̶_̶_̶_̶_̶_̶_̶_̶_̶_̶_̶_̶_̶_̶_̶" yellow
 done
+printf "\n"
+texColor "▄▄ ▄▄ ▄▄ ▄▄ ▄▄ ▄▄ ▄▄ ▄▄ ▄▄ ▄▄ ▄▄ ▄▄ ▄▄ ▄▄ ▄▄ ▄▄ ▄▄ ▄▄ ▄▄ ▄▄ ▄▄ ▄▄ ▄▄ ▄▄ ▄▄ ▄▄ ▄▄ ▄▄ " blue
+printf "\n"
+texColor "
+█▀ █▄░█ ▄▀█ █▀█
+▄█ █░▀█ █▀█ █▀▀
+" purple
 
-# SNAP
 arry2=('bottom' 'speedtest-cli' 'cpufetch')
 
 for i in "${arry2[@]}"
 do
-    echo -e "\e[31m  Programa a Intalar ${i}\e[0m"
+    printf "\n\n"
+    texColor " Programa a Intalar ${i}" green
+    printf "\n\n"
     snap install ${i}
-    echo -e "\e[1;34m <------------------------>\e[0m"
+    printf "\n\n"
+    texColor "_̶_̶_̶_̶_̶_̶_̶_̶_̶_̶_̶_̶_̶_̶_̶_̶_̶_̶_̶_̶_̶_̶_̶_̶_̶_̶_̶_̶_̶_̶_̶_̶_̶_̶_̶_̶_̶_̶_̶_̶_̶_̶_̶_̶_̶_̶_̶_̶_̶_̶_̶_̶_̶_̶_̶_̶_̶_̶_̶_̶_̶_̶_̶" yellow
 done
+printf "\n"
+texColor "▄▄ ▄▄ ▄▄ ▄▄ ▄▄ ▄▄ ▄▄ ▄▄ ▄▄ ▄▄ ▄▄ ▄▄ ▄▄ ▄▄ ▄▄ ▄▄ ▄▄ ▄▄ ▄▄ ▄▄ ▄▄ ▄▄ ▄▄ ▄▄ ▄▄ ▄▄ ▄▄ ▄▄ " blue
+printf "\n"
+texColor "
+█▀▀ █░░ ▄▀█ ▀█▀ █▀█ ▄▀█ █▄▀
+█▀░ █▄▄ █▀█ ░█░ █▀▀ █▀█ █░█
+" purple
 
-# FLATPAK
 arry3=('telegram')
 
 for i in "${arry3[@]}"
 do
-    echo -e "\e[31m  Programa a Intalar ${i}\e[0m"
+    printf "\n\n"
+    texColor " Programa a Intalar ${i}" green
+    printf "\n\n"
     flatpak install ${i} -y
-    echo -e "\e[1;34m <------------------------>\e[0m"
+    printf "\n\n"
+    texColor "_̶_̶_̶_̶_̶_̶_̶_̶_̶_̶_̶_̶_̶_̶_̶_̶_̶_̶_̶_̶_̶_̶_̶_̶_̶_̶_̶_̶_̶_̶_̶_̶_̶_̶_̶_̶_̶_̶_̶_̶_̶_̶_̶_̶_̶_̶_̶_̶_̶_̶_̶_̶_̶_̶_̶_̶_̶_̶_̶_̶_̶_̶_̶" yellow
 done
+printf "\n"
+texColor "▄▄ ▄▄ ▄▄ ▄▄ ▄▄ ▄▄ ▄▄ ▄▄ ▄▄ ▄▄ ▄▄ ▄▄ ▄▄ ▄▄ ▄▄ ▄▄ ▄▄ ▄▄ ▄▄ ▄▄ ▄▄ ▄▄ ▄▄ ▄▄ ▄▄ ▄▄ ▄▄ ▄▄ " blue
